37ac96e92a252133fe4edfa127a34356c67b69c5
Vlad_Kryvokoniev <Dlfacheater2@gmail.com>
2020-08-31 19:52:47 +0300

    add eslint

--- src/components/other/Other.tsx
+++ src/components/other/Other.tsx
@@ -3,7 +3,7 @@ import { Wrapper, StyledButton } from './other.style';
 import { Fact } from '../../utils/types';
 
 export default function Other() {
-  const [fact, setFact] = useState({});
+  const [ fact, setFact ] = useState({});
 
   const getFact: (url: string) => Promise<Fact> = (url) => {
     return fetch(url).then((response) => response.json());
@@ -11,19 +11,19 @@ export default function Other() {
 
   const onGetFact: () => void = async () => {
     setFact(
-      await getFact("https://uselessfacts.jsph.pl/random.json?language=en")
+      await getFact('https://uselessfacts.jsph.pl/random.json?language=en')
     );
   };
 
   return (
-    <Wrapper>
-      <h1>Testing get request</h1>
-      <div>
-        <StyledButton onClick={onGetFact}>Get random fact!</StyledButton>
-        <div>
-          <h3>{(fact as Fact).text}</h3>
-        </div>
-      </div>
-    </Wrapper>
+      <Wrapper>
+          <h1>Testing get request</h1>
+          <div>
+              <StyledButton onClick={ onGetFact }>Get random fact!</StyledButton>
+              <div>
+                  <h3>{(fact as Fact).text}</h3>
+              </div>
+          </div>
+      </Wrapper>
   );
 }
