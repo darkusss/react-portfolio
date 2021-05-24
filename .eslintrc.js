e010fbda2615d528d26693e5c9c61d42a4281933
darkusss <dlfacheater2@gmail.com>
2021-05-24 22:32:28 +0300

    fix: add react plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     },
     plugins: [
         "babel",
+        "react",
     ],
     extends: [
         "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
